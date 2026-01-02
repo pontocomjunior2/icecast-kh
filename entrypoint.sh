8f3dee7889c2936286293c8ea7855bb4954b5465
Junior <junior@example.com>
2026-01-02 12:10:03 -0300

    Security: Remove sensitive hardcoded credentials and prioritize environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 # Substituir variáveis de ambiente no template do icecast.xml
 # Se a variável não estiver definida, o envsubst deixará vazio ou você pode definir padrões aqui
-export ICECAST_SOURCE_PASSWORD=${ICECAST_SOURCE_PASSWORD:-hackme}
-export ICECAST_RELAY_PASSWORD=${ICECAST_RELAY_PASSWORD:-hackme}
-export ICECAST_ADMIN_USER=${ICECAST_ADMIN_USER:-admin}
-export ICECAST_ADMIN_PASSWORD=${ICECAST_ADMIN_PASSWORD:-hackme}
+export ICECAST_SOURCE_PASSWORD=${ICECAST_SOURCE_PASSWORD}
+export ICECAST_RELAY_PASSWORD=${ICECAST_RELAY_PASSWORD}
+export ICECAST_ADMIN_USER=${ICECAST_ADMIN_USER}
+export ICECAST_ADMIN_PASSWORD=${ICECAST_ADMIN_PASSWORD}
 export ICECAST_HOSTNAME=${ICECAST_HOSTNAME:-localhost}
 export ICECAST_LOCATION=${ICECAST_LOCATION:-Brazil}
 export ICECAST_ADMIN_EMAIL=${ICECAST_ADMIN_EMAIL:-admin@localhost}
